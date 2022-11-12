ba851629e81e6e9c1c7c47839b5bd17152f38e2c
Krzysztof Borecki <80486337+K3orecki@users.noreply.github.com>
2022-11-12 01:38:27 +0100

    Add State and Effect hooks in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 // import React from 'react';
 import axios from 'axios';
-import { ReactElement } from 'react';
+import { ReactElement, useState, useEffect } from 'react';
 
 import { searchUrl, testUrl } from './Search';
 
@@ -20,30 +20,143 @@ const TEST_DATA = {
   text: TEST_TEXT,
 }
 
+const TEST_MAIN_CATEGORIES = [
+  'News',
+  'Opinion',
+  'Sport',
+  'Culture',
+  'Lifestyle',
+];
+
+const TEST_CATEGORIES = [
+  {
+    category: 'News',
+    subcategories: [
+      'World news',
+      'UK news',
+      'Coronavirus',
+      'Climate crisis',
+      'Environment',
+      'Science',
+      'Global development',
+      'Football',
+      'Tech',
+      'Business',
+      'Obituaries',
+    ],
+  },
+  {
+    category: 'Opinion',
+    subcategories: [
+      'The Guardian view',
+      'Columnists',
+      'Cartoons',
+      'Opinion videos',
+      'Letters',
+    ],
+  },
+  {
+    category: 'Sport',
+    subcategories: [
+      'Football',
+      'Cricket',
+      'Rugby union',
+      'Tennis',
+      'Cycling',
+      'F1',
+      'Golf',
+      'US sports',
+    ],
+  },
+  {
+    category: 'Culture',
+    subcategories: [
+      'Music',
+      'Books',
+      'TV & radio',
+      'Art & design',
+      'Film',
+      'Games',
+      'Classical',
+      'Stage',
+    ],
+  },
+  {
+    category: 'Lifestyle',
+    subcategories: [
+      'Fashion',
+      'Food',
+      'Recipes',
+      'Love & sex',
+      'Health & fitness',
+      'Home & garden',
+      'Women',
+      'Men',
+      'Family',
+      'Travel',
+      'Money',
+    ],
+  },
+];
+
+console.log(TEST_CATEGORIES);
+
+const TEST_DATA_ARRAY = [TEST_DATA, TEST_DATA, TEST_DATA];
+
 const getDateFormatEU = (value: string) => `${value[2]}/${value[1]}/${value[0]}`;
 const getDateFormatUS = (value: string) => `${value[1]}/${value[2]}/${value[0]}`;
 
 //__TEST URL
 // const url = `https://content.guardianapis.com/search?q=debate&tag=politics/politics&from-date=2014-01-01&${API_KEY_URL}`;
 
-const getResponseByAxios = async (url: string) => {
-  const response = await axios.get(url);
-  // tu już mam obiekt
-  console.log(response);
-  return response;
-  // tu funkcja - zwraca Promise z value: <wartość zmiennej response>
-}
-getResponseByAxios(searchUrl);
-console.log(getResponseByAxios(searchUrl));
+// const getResponseByAxios = async (url: string) => {
+//   // try {
+//     // const response = await axios.get(url);
+//      const response = await axios.get(url);
+//     // tu już mam obiekt
+//     // console.log(response);
+//     return response;
+//     // tu funkcja - zwraca Promise z value: <wartość zmiennej response>
+//   // } catch (err) {
+//   //   console.log(err);
+//   // }
+// }
+// getResponseByAxios(searchUrl);
+// console.log(getResponseByAxios(searchUrl));
+
+
 
 export default function App(): ReactElement {
+
+  const [state, setState]: [any | undefined, any] = useState(null);
+
+  //__TEST URL
+  // const url = `https://content.guardianapis.com/search?q=debate&tag=politics/politics&from-date=2014-01-01&${API_KEY_URL}`;
+
+  // getResponseByAxios === <Promise> 'resolved' z <value> === () => retur <value>
+  const getResponseByAxios = async (url: string) => {
+    try {
+      const response = await axios.get(url);
+      // reposnes === <obiekt>      
+      setState(response);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  useEffect(() => {
+    getResponseByAxios(searchUrl);
+  }, []);
+
+  console.log(state);
+
   return (
-    <div>
-      <Sidebar />
-      <Results 
+    <div className='app'>
+      <Sidebar categories={TEST_MAIN_CATEGORIES} />
+      <Results
         title={PAGE_TITLE}
-        data={TEST_DATA}
+        data={TEST_DATA_ARRAY}
       />
-    </div>	
+    </div>
   );
 }
