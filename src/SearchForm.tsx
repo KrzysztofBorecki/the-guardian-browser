84611fcb8a7f2efe77cae0e767ce00b0139ab10b
Krzysztof Borecki <80486337+K3orecki@users.noreply.github.com>
2022-11-16 03:21:50 +0100

    Move eu/us date formatting to SearchForm.tsx

--- src/SearchForm.tsx
+++ src/SearchForm.tsx
@@ -1,5 +1,8 @@
 import { ReactElement } from 'react';
 
+const getDateFormatEU = (value: string) => `${value[2]}/${value[1]}/${value[0]}`;
+const getDateFormatUS = (value: string) => `${value[1]}/${value[2]}/${value[0]}`;
+
 export default function SearchForm() {
     return (
         <div>Search + Submit btn</div>
