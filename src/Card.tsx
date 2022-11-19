4787dcec0ac68c7d622b860d2bad0e4b8e561bd3
Krzysztof Borecki <80486337+K3orecki@users.noreply.github.com>
2022-11-19 06:45:08 +0100

    Fix TS Types; Add read more btn, author info

--- src/Card.tsx
+++ src/Card.tsx
@@ -1,29 +1,36 @@
 import { ReactElement } from 'react';
+import arrowRight from './arrow-right.svg';
+import { Author, CardData } from './Types';
 
-interface PropsCard {
-    title: string;
-    date: string;
-    tag: string;
-    text: string;
-}
+// import { Link } from 'react-router-dom';
 
-export default function Card(props: PropsCard): ReactElement {
+export default function Card(props: CardData): ReactElement {
     return (
         <div className='card'>
+            <div className='card-info'>
+                <p className='card-info-tag'>
+                    {props.tag.toUpperCase()}
+                </p>          
+                <p className='card-info-date'>
+                    {props.date}
+                </p>
+            </div>
             <h2 className='card-title'>
                 {props.title}
             </h2>
-            <div className='card-info'>
-                <p>
-                    {`Date: ${props.date}`}
-                </p>
-                <p>
-                    {`Tags: ${props.tag}`}
-                </p>          
-            </div>
             <p className='card-text'>
                 {props.text}
             </p>
+            <div className='card-info'>
+                <a className='card-info-author' href='/'>
+                    <img className='avatar' src={props.author.avatar} alt="avatar" />
+                    <p className='name'>{props.author.name}</p>
+                </a>
+                <a className='card-info-link' href={`https://www.theguardian.com`}>
+                    Read more
+                    <img src={arrowRight} className={'arrow-right'} alt="arrow right" />
+                </a>
+            </div>
         </div>
     );
 }
