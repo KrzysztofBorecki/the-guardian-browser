4a8f0172bd612daaa05558adfd6b7e1ff90466cd
Krzysztof Borecki <80486337+K3orecki@users.noreply.github.com>
2022-11-19 06:22:44 +0100

    Add SearchForm props, TS Types

--- src/Sidebar.tsx
+++ src/Sidebar.tsx
@@ -1,12 +1,18 @@
 import { ReactElement } from 'react';
 import SearchForm from './SearchForm';
 import Categories from './Categories';
+import { SidebarProps } from './Types';
 
-export default function Sidebar(): ReactElement {
+export default function Sidebar(props: SidebarProps): ReactElement {
     return (
         <div className='sidebar'>
-            <SearchForm/>
-            <Categories/>
+            <SearchForm 
+                onChange={props.onChange} 
+                onSubmit={props.onSubmit} 
+                onReset={props.onReset} 
+                searchQuery={props.searchQuery}
+            />
+            <Categories sectionsData={props.sectionsData}/>
         </div>
     )
 }
