d48a6809b02e18e6840f4a2dd532c0f3a3885af9
Krzysztof Borecki <80486337+K3orecki@users.noreply.github.com>
2022-11-19 06:19:15 +0100

    Add search form elements

--- src/SearchForm.tsx
+++ src/SearchForm.tsx
@@ -1,10 +1,42 @@
 import { ReactElement } from 'react';
+import { SearchFormProps } from './Types';
 
 const getDateFormatEU = (value: string) => `${value[2]}/${value[1]}/${value[0]}`;
 const getDateFormatUS = (value: string) => `${value[1]}/${value[2]}/${value[0]}`;
 
-export default function SearchForm() {
+export default function SearchForm(props: SearchFormProps): ReactElement {
     return (
-        <div>Search + Submit btn</div>
+        <>
+            <form onSubmit={props.onSubmit} onReset={props.onReset} className='form'>
+                <div className='form-panel'>
+                    <label htmlFor='search' hidden={true}>Search</label>
+                    <input 
+                        id='search' 
+                        type='text' 
+                        name='search' 
+                        value={props.searchQuery} 
+                        onChange={props.onChange} 
+                        placeholder='Search...'
+                        className='form-search'
+                    />
+                    <label htmlFor='submit' hidden={true}>Submit</label>
+                    <input 
+                        id='submit' 
+                        type='submit' 
+                        name='submit' 
+                        value="Find"
+                        className='form-submit'
+                    />
+                </div>
+                <label htmlFor='reset' hidden={true}>All articles</label>
+                <input 
+                    id='reset' 
+                    type='reset' 
+                    name='reset' 
+                    value='All articles'
+                    className='form-reset'
+                />
+            </form>
+        </>
     );
 }
