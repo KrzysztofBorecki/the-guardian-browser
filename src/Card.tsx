a14ebac6e3983372fb2b73e1f3ed8945ca5b8a49
Krzysztof Borecki <80486337+K3orecki@users.noreply.github.com>
2022-11-21 02:55:44 +0100

    Add usage of server articles data

--- src/Card.tsx
+++ src/Card.tsx
@@ -1,36 +1,65 @@
 import { ReactElement } from 'react';
 import arrowRight from './arrow-right.svg';
-import { Author, CardData } from './Types';
+import type { CardData } from './Types';
+
+// function getParsedDate(dateString: string): string {
+//     const dateMs = Date.parse(dateString);
+//     const dateObj = new Date(dateMs);
+//     const year = dateObj.getFullYear();
+//     const month = dateObj.getMonth() + 1;
+//     const day = dateObj.getDate();
+    
+//     return `${day}-${month}-${year}`;
+// }
+
+function getDateString(value: string): string {
+    return new Date(value).toString();
+};
+
+function getDateParamList(value: string): string[] {
+    return value.split(' ');
+}
+
+function getArticleDate(value: string[]): string {
+    return `${value[2]} ${value[1]} ${value[3]}`;
+}
+
+function getParsedDate(value: string): string {
+    const dateString = getDateString(value);
+    const dateSplitted = getDateParamList(dateString);
+
+    return getArticleDate(dateSplitted);
+}
 
 // import { Link } from 'react-router-dom';
 
 export default function Card(props: CardData): ReactElement {
     return (
-        <div className='card'>
+        <a className='card' href={props.webUrl}>
             <div className='card-info'>
                 <p className='card-info-tag'>
-                    {props.tag.toUpperCase()}
+                    {props.sectionName.toUpperCase()}
                 </p>          
                 <p className='card-info-date'>
-                    {props.date}
+                    {getParsedDate(props.webPublicationDate)}
                 </p>
             </div>
             <h2 className='card-title'>
-                {props.title}
+                {props.webTitle}
             </h2>
             <p className='card-text'>
-                {props.text}
+                {'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ea suscipit fuga quisquam eos, quaerat non at eum eius quia ut deserunt sed, aliquid qui explicabo totam nulla velit modi vitae.'}
             </p>
             <div className='card-info'>
                 <a className='card-info-author' href='/'>
                     <img className='avatar' src={props.author.avatar} alt="avatar" />
                     <p className='name'>{props.author.name}</p>
                 </a>
-                <a className='card-info-link' href={`https://www.theguardian.com`}>
+                <a className='card-info-link' href={props.webUrl}>
                     Read more
                     <img src={arrowRight} className={'arrow-right'} alt="arrow right" />
                 </a>
             </div>
-        </div>
+        </a>
     );
 }
