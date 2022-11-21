479b78a421de15eb396720a3740277d4b38534bd
Krzysztof Borecki <80486337+K3orecki@users.noreply.github.com>
2022-11-21 02:54:20 +0100

    Add avatars jpg, authors data

--- src/Data.tsx
+++ src/Data.tsx
@@ -1,13 +1,24 @@
-import avatar01 from './jpg/avatar-01.jpg';
-import avatar02 from './jpg/avatar-02.jpg';
-import avatar03 from './jpg/avatar-03.jpg';
-import avatar04 from './jpg/avatar-04.jpg';
-import avatar05 from './jpg/avatar-05.jpg';
-import avatar06 from './jpg/avatar-06.jpg';
-import avatar07 from './jpg/avatar-07.jpg';
-import avatar08 from './jpg/avatar-08.jpg';
-import avatar09 from './jpg/avatar-09.jpg';
-import avatar10 from './jpg/avatar-10.jpg';
+import avatar01 from './jpg/named-10/avatar-01-philip-martin-unsplash.jpg';
+import avatar02 from './jpg/named-10/avatar-02-jonathan-borba-unsplash.jpg';
+import avatar03 from './jpg/named-10/avatar-03-gelmis-bartulis-unsplash.jpg';
+import avatar04 from './jpg/named-10/avatar-04-michael-dam-unsplash.jpg';
+import avatar05 from './jpg/named-10/avatar-05-andrey-zvyagintsev-unsplash.jpg';
+import avatar06 from './jpg/named-10/avatar-06-rachel-mcdermott-unsplash.jpg';
+import avatar07 from './jpg/named-10/avatar-07-d-a-v-i-d-s-o-n-l-u-n-a-unsplash.jpg';
+import avatar08 from './jpg/named-10/avatar-08-jonathan-cosens-photography-unsplash.jpg';
+import avatar09 from './jpg/named-10/avatar-09-christina-wocintechchat-com-unsplash.jpg';
+import avatar010 from './jpg/named-10/avatar-10-linkedin-sales-solutions-unsplash.jpg';
+
+// import avatar01 from './jpg/avatar-01.jpg';
+// import avatar02 from './jpg/avatar-02.jpg';
+// import avatar03 from './jpg/avatar-03.jpg';
+// import avatar04 from './jpg/avatar-04.jpg';
+// import avatar05 from './jpg/avatar-05.jpg';
+// import avatar06 from './jpg/avatar-06.jpg';
+// import avatar07 from './jpg/avatar-07.jpg';
+// import avatar08 from './jpg/avatar-08.jpg';
+// import avatar09 from './jpg/avatar-09.jpg';
+// import avatar10 from './jpg/avatar-10.jpg';
 
 export const URL_BASE = 'https://content.guardianapis.com/';
 export const URL_SEARCH = '/search';
@@ -116,26 +127,58 @@ export const TEST_MAIN_CATEGORIES = [
 export const TEST_AUTHORS_BASE = [
     {
         name: 'Edward Newgate',
-        avatar: avatar01 
+        avatar: avatar01, 
     },
     {
         name: 'Alisa Bosii',
-        avatar: avatar02 
+        avatar: avatar02,
     },
     {
         name: 'Thomas Mazer',
-        avatar: avatar03 
+        avatar: avatar03, 
     },
     {
-        name: 'Anastasia Bergsen',
-        avatar: avatar04 
+        name: 'Anastasia Dumitru',
+        avatar: avatar04, 
     },  
+    {
+        name: 'Astrid Giraud',
+        avatar: avatar05,
+    },
+    {
+        name: 'Katrijn Rey',
+        avatar: avatar06,
+    },
+    {
+        name: 'Joakim Arnaud',
+        avatar: avatar07,
+    },
+    {
+        name: 'Léon Guillot',
+        avatar: avatar08,
+    },
+    {
+        name: 'Marion Salina',
+        avatar: avatar09,
+    },
+    {
+        name: 'Gustav Filipek',
+        avatar: avatar010,
+    },
 ]
 
+//__orig - for 4 base avatars
+// export function getAuthors() {
+//     const authorsList = TEST_AUTHORS_BASE;
+//     return Array.from(new Array(10).fill(null), () => authorsList[Math.floor(Math.random() * 4)])
+// }
+
+//__new - for 10 avatars
 export function getAuthors() {
     const authorsList = TEST_AUTHORS_BASE;
-    return Array.from(new Array(10).fill(null), () => authorsList[Math.floor(Math.random() * 4)])
+    return Array.from(new Array(10).fill(null), () => authorsList[Math.floor(Math.random() * 10)])
 }
+
 
 // export const TEST_AUTHORS = [
 //     {
@@ -184,12 +227,14 @@ const TEST_CARD_TITLE = 'Test Article Title';
 const TEST_CARD_DATE_EU = '24/10/2022';
 const TEST_CARD_TAG = 'politics';
 const TEST_CARD_TEXT = 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ea suscipit fuga quisquam eos, quaerat non at eum eius quia ut deserunt sed, aliquid qui explicabo totam nulla velit modi vitae.'
+const TEST_CARD_URL = 'https://www.google.com'
 
 const TEST_CARD_DATA = {
     title: TEST_CARD_TITLE,
     date: TEST_CARD_DATE_EU,
     tag: TEST_CARD_TAG,
     text: TEST_CARD_TEXT,
+    url: TEST_CARD_URL,
     // author: TEST_AUTHORS[0], 
 }
 
