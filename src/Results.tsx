4a1c771138707d44e39fd8afd87eb0020005432c
Krzysztof Borecki <80486337+K3orecki@users.noreply.github.com>
2022-11-22 18:58:58 +0100

    Add pagination

--- src/Results.tsx
+++ src/Results.tsx
@@ -1,25 +1,33 @@
 import { ReactElement } from 'react';
 import Card from './Card';
 import Pagination from './Pagination';
-
-import type { ResultsData} from './Types';
+import type { ResultsData } from './Types';
 import type { ResponseArticlesResults } from './AxiosArticles';
 
 export default function Results(props: ResultsData): ReactElement {
     const articles = props.data.results;
     const items = articles.map((obj: ResponseArticlesResults, idx: number) =>
-        <Card key={obj.webTitle} {...obj} author={props.authors[idx]} />
+        <Card
+            key={obj.id} {...obj}
+            author={props.authors[idx]}
+        />
     );
 
     return (
         <div className='results'>
             <h1 className='page-title'>
                 {props.title}
-            </h1>    
+            </h1>
             <div className='articles'>
                 {items}
             </div>
-            <Pagination pagesAll={props.data.pages} pagesCurrent={props.data.currentPage}/>
+            <Pagination
+                pagesAll={props.data.pages}
+                pagesCurrent={props.data.currentPage}
+                onClick={props.onClick}
+                onPageUp={props.onPageUp}
+                onPageDown={props.onPageDown}
+            />
         </div>
     );
 }
