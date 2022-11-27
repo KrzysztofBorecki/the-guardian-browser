c6d34231670b6f473bc658b8952609d661d68b49
Krzysztof Borecki <80486337+K3orecki@users.noreply.github.com>
2022-11-27 03:13:26 +0100

    Add types from Axios modules to Types module

--- src/Types.tsx
+++ src/Types.tsx
@@ -1,5 +1,39 @@
-import type { ResponseSectionsResults } from './AxiosSections';
-import type { ResponseArticlesResults, ResponseArticles } from './AxiosArticles';
+// import type { ResponseSectionsResults } from './AxiosSections';
+// import type { ResponseArticlesResults, ResponseArticles } from './AxiosArticles';
+
+
+export type ResponseSectionsResults = {
+    apiUrl: string;
+    id: string;
+    webTitle: string;
+    webUrl: string;
+}
+
+export type ResponseArticlesResults = {
+    id:	string;
+    type: string;
+    sectionId: string;
+    sectionName: string;
+    webPublicationDate: string;
+    webTitle: string;
+    webUrl:	string;
+    apiUrl:	string;
+    isHosted: boolean;
+    pillarId: string;
+    pillarName: string;
+}
+
+type ResponseArticles = {
+    status:	string;
+    userTier: string;
+    total:	number;
+    startIndex:	number;
+    pageSize: number;
+    currentPage: number;
+    pages: number;
+    orderBy: string;
+    results: ResponseArticlesResults[]
+}
 
 //__App/Results
 export interface Author {
