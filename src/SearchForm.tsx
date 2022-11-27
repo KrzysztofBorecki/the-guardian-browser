2045ff96a5925a395d529ddb0df3969419906123
Krzysztof Borecki <80486337+K3orecki@users.noreply.github.com>
2022-11-27 03:16:06 +0100

    Add local state; Add Change/Submit/Reset handlers

--- src/SearchForm.tsx
+++ src/SearchForm.tsx
@@ -1,21 +1,38 @@
-import { ReactElement } from 'react';
+import React, { ReactElement, useState } from 'react';
 import { SearchFormProps } from './Types';
 
-const getDateFormatEU = (value: string) => `${value[2]}/${value[1]}/${value[0]}`;
-const getDateFormatUS = (value: string) => `${value[1]}/${value[2]}/${value[0]}`;
+// const getDateFormatEU = (value: string) => `${value[2]}/${value[1]}/${value[0]}`;
+// const getDateFormatUS = (value: string) => `${value[1]}/${value[2]}/${value[0]}`;
 
 export default function SearchForm(props: SearchFormProps): ReactElement {
+    const [searchPhrase, setSearchPhrase] = useState<string>('');
+
+    function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+        setSearchPhrase(event.target.value);
+        event.preventDefault();
+    }
+
+    function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+        props.onSubmit(searchPhrase);
+        event.preventDefault();
+    }
+
+    function handleReset(event: React.FormEvent<HTMLFormElement>) {
+        setSearchPhrase('');
+        props.onReset();
+        event.preventDefault();    
+    }
     return (
         <>
-            <form onSubmit={props.onSubmit} onReset={props.onReset} className='form'>
+            <form onSubmit={handleSubmit} onReset={handleReset} className='form'>
                 <div className='form-panel'>
                     <label htmlFor='search' hidden={true}>Search</label>
                     <input 
                         id='search' 
                         type='text' 
                         name='search' 
-                        value={props.searchQuery} 
-                        onChange={props.onChange} 
+                        value={searchPhrase} 
+                        onChange={handleChange} 
                         placeholder='Search...'
                         className='form-search'
                     />
