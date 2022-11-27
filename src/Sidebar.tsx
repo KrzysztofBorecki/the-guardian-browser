b0bb2c82833ff2a7abeda5aa2efe81be3f8605d6
Krzysztof Borecki <80486337+K3orecki@users.noreply.github.com>
2022-11-27 03:02:36 +0100

    Remove redundant props

--- src/Sidebar.tsx
+++ src/Sidebar.tsx
@@ -1,18 +1,16 @@
 import { ReactElement } from 'react';
 import SearchForm from './SearchForm';
 import Categories from './Categories';
-import { SidebarProps } from './Types';
+import type { SidebarProps } from './Types';
 
 export default function Sidebar(props: SidebarProps): ReactElement {
     return (
         <div className='sidebar'>
             <SearchForm 
-                onChange={props.onChange} 
                 onSubmit={props.onSubmit} 
                 onReset={props.onReset} 
-                searchQuery={props.searchQuery}
             />
             <Categories sectionsData={props.sectionsData}/>
         </div>
-    )
+    );
 }
