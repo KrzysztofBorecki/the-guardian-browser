ab8d783169807077bf2c399f583d125cd4fd1156
Krzysztof Borecki <80486337+K3orecki@users.noreply.github.com>
2022-11-30 23:06:29 +0100

    Add 'no results' message

--- src/Results.tsx
+++ src/Results.tsx
@@ -6,13 +6,19 @@ import type { ResultsData } from './Results.types';
 
 export default function Results(props: ResultsData): ReactElement {
     const articles = props.data.results;
-    const items = articles.map((obj: ResponseSearchResults, idx: number) =>
-        <Card
-            key={obj.id} {...obj}
-            author={props.authors[idx]}
-        />
+
+    const items = (
+        (articles.length) ? 
+        articles.map((obj: ResponseSearchResults, idx: number) =>
+            <Card
+                key={obj.id} {...obj}
+                author={props.authors[idx]}
+            />   
+        ) :
+        <strong className='no-results'>Sorry! No results found!</strong>
     );
 
+    console.log(items);
     return (
         <div className='results'>
             <h1 className='page-title'>
