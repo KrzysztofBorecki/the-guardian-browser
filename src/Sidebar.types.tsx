700d16ee82ca26145e87be3a27fd42b3967ddef8
Krzysztof Borecki <80486337+K3orecki@users.noreply.github.com>
2022-11-30 03:33:02 +0100

    Update Sidebar types

--- src/Sidebar.types.tsx
+++ src/Sidebar.types.tsx
@@ -3,5 +3,7 @@ import type { ResponseSectionsResults } from './Types';
 export interface SidebarProps {
     onSubmit: (searchPhrase: string) => void;
     onReset: () => void;
+    onClick: (section: string) => void;
     sectionsData: ResponseSectionsResults[];
+    searchParams: URLSearchParams;
 }
