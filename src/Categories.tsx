c97195e04d3d1b22fe5c41ed32abaada3ab391c6
Krzysztof Borecki <80486337+K3orecki@users.noreply.github.com>
2022-11-30 03:26:21 +0100

    Add section pick handling

--- src/Categories.tsx
+++ src/Categories.tsx
@@ -4,9 +4,18 @@ import type { SectionsData } from './Categories.types';
 
 export default function Categories(props: SectionsData): ReactElement {
     const sectionsData = props.sectionsData;
-
+    const searchParams = props.searchParams;
+    const currentSection = searchParams.get('section');
     const items = sectionsData.map((section: ResponseSectionsResults) =>
-        <li key={section.id} className='main-category' data-href={section.webUrl}>
+        <li 
+            key={section.id} 
+            className='main-category' 
+            data-href={section.webUrl}
+            onClick={() => {
+                props.onClick(section.id);
+            }}
+            data-selected={(section.id === currentSection) ? true : false}
+        >
             {section.webTitle}
         </li>
     );
