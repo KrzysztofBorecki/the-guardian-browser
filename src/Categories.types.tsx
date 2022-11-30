6b9ebbaeeebdf4c00d29d4c3a5189b61353dc870
Krzysztof Borecki <80486337+K3orecki@users.noreply.github.com>
2022-11-30 03:26:55 +0100

    Update Categories module types

--- src/Categories.types.tsx
+++ src/Categories.types.tsx
@@ -2,4 +2,6 @@ import type { ResponseSectionsResults } from './Types';
 
 export interface SectionsData {
     sectionsData: ResponseSectionsResults[];
+    onClick: (section: string) => void;
+    searchParams: URLSearchParams;
 }
